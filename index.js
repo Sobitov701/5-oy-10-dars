176a2024f130ea358deaa1b54ed681d81417f8cd
Sobitov701 <abdurasul03sobitaliyev>
2025-01-16 15:55:07 +0300

    all add

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const emojis = ["😀", "🎉", "🚀", "🐱", "🍎"];
 const button = document.getElementById("emoji");
 const emojiDisplay = document.getElementById("emoji-display");
 
-button.addEventListener("click", () => {
-  const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-  emojiDisplay.textContent = randomEmoji;
-});
+button &&
+  button.addEventListener("click", () => {
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    emojiDisplay.textContent = randomEmoji;
+  });
 
 //2-misol
 //Bir <textarea> va bir nechta tugmalar yarating:
@@ -24,36 +25,38 @@ const boldBtn = document.getElementById("bold-btn");
 const italicBtn = document.getElementById("italic-btn");
 const clearBtn = document.getElementById("clear-btn");
 
-boldBtn.addEventListener("click", () => {
-  textarea.style.fontWeight = "bold";
-});
+boldBtn &&
+  boldBtn.addEventListener("click", () => {
+    textarea.style.fontWeight = "bold";
+  });
 
-italicBtn.addEventListener("click", () => {
-  textarea.style.fontStyle = "italic";
-});
+italicBtn &&
+  italicBtn.addEventListener("click", () => {
+    textarea.style.fontStyle = "italic";
+  });
 
-clearBtn.addEventListener("click", () => {
-  textarea.value = "";
-});
+clearBtn &&
+  clearBtn.addEventListener("click", () => {
+    textarea.value = "";
+  });
 
 //3-misol
 //Bir tugma (Start) va bir progress bar (<div> elementi ichida progressni ko‘rsatuvchi boshqa <div> elementi) yarating.
 //Tugmani bosganda progress bar sekin-asta to‘ladi (masalan, har 100ms 5% ga o‘sib boradi).
 //Progress bar 100% ga to‘lganda tugma ishlamay qoladi.
 
-const startBtn = document.getElementById("start-btn");
+const start = document.getElementById("start-btn");
 const progress = document.getElementById("progress");
 
 let num = 0;
 
-startBtn.addEventListener("click", () => {
-  startBtn.disabled = true;
-
-  const interval = setInterval(() => {
-    num += 5;
-    progress.style.width = num + "%";
-    if (num >= 100) {
-      clearInterval(interval);
-    }
-  }, 100);
-});
+start &&
+  start.addEventListener("click", () => {
+    const interval = setInterval(() => {
+      num += 5;
+      progress.style.width = num + "%";
+      if (num >= 100) {
+        clearInterval(interval);
+      }
+    }, 100);
+  });
